4bed4cc80b962a391deb76b8b427acf13b8e9ac2
Fernando B.G <fbgranell@gmail.com>
2023-03-25 08:55:49 +0100

    data augmentation + training

--- python/2_Training.py
+++ python/2_Training.py
@@ -13,26 +13,25 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+size = 224
 
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,  # rescale the pixel values to [0, 1]
     #shear_range=0.2,  # apply random shearing
-    #zoom_range=0.2,  # apply random zooming
-    #horizontal_flip=True  # randomly flip the images horizontally
+    zoom_range=0.2,  # apply random zooming
+    horizontal_flip=True  # randomly flip the images horizontally
 )
-
 train_generator = train_datagen.flow_from_directory(
     directory='../data/train',
-    target_size=(224, 224),
+    target_size=(size, size),
     batch_size=32,
     class_mode='categorical'
 )
 
 val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-
 val_generator = val_datagen.flow_from_directory(
     directory='../data/valid',
-    target_size=(224, 224),
+    target_size=(size, size),
     batch_size=32,
     class_mode='categorical'
 )
@@ -41,60 +40,60 @@ def inception_module(input_,filters_1x1,filters_3x3_pre, filters_3x3, filters_5x
 
     # Inception block
     conv_1x1 = Conv2D(filters_1x1, 1, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(input_)
 
     conv_3x3 = Conv2D(filters_3x3_pre, 1, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(input_)
     conv_3x3 = Conv2D(filters_3x3, 3, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(conv_3x3)
 
     conv_5x5 = Conv2D(filters_5x5_pre, 1, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(input_)
     conv_5x5 = Conv2D(filters_5x5, 3, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(conv_5x5)
 
     max_pool = MaxPool2D(pool_size=(3, 3),strides=(1,1),padding='same')(input_)
     max_pool = Conv2D(filters_maxpool, 1, strides=(1, 1), padding='same', activation='relu',
-                      kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10),
+                      kernel_initializer=tf.keras.initializers.glorot_uniform(),
                       bias_initializer=bias_initializer)(max_pool)
 
     output =  tf.keras.layers.concatenate([conv_1x1, conv_3x3, conv_5x5, max_pool], axis=3, name=name)
     return output
 
 # Initializers
-bias_initializer = tf.keras.initializers.Constant(value=0.)
+bias_initializer = tf.keras.initializers.Constant(value=0.2)
 
-# GOOLENET ARCHITECTURE
+
+# Neural Network
 n_categories = 510
+input_layer =  Input(shape=(size,size,3))
 
-input_layer =  Input(shape=(224,224,3))
 
-
-# B L O C K - 1
+# BLOCK - 1
 x = Conv2D(64, (7, 7), padding='same', strides=(2, 2), activation='relu', name='conv_1_7x7',
-           kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10), bias_initializer=bias_initializer)(input_layer)
+           kernel_initializer=tf.keras.initializers.glorot_uniform(), bias_initializer=bias_initializer)(input_layer)
 
 x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_1_3x3')(x)
 #x = tf.nn.local_response_normalization(x, name ='norm1')
 
 
-# B L O C K - 2
+# BLOCK - 2
 x = Conv2D(64, (1, 1), padding='same', strides=(1, 1), activation='relu', name='conv_2a_1x1',
-           kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10), bias_initializer=bias_initializer)(x)
+           kernel_initializer=tf.keras.initializers.glorot_uniform(), bias_initializer=bias_initializer)(x)
 
 x = Conv2D(192, (3, 3), padding='same', strides=(1, 1), activation='relu', name='conv_2b_3x3',
-           kernel_initializer=tf.keras.initializers.glorot_uniform(seed=10), bias_initializer=bias_initializer)(x)
+           kernel_initializer=tf.keras.initializers.glorot_uniform(), bias_initializer=bias_initializer)(x)
 
 #x = tf.nn.local_response_normalization(x, name ='norm2')
 x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_2_3x3')(x)
 
 
-# B L O C K - 3 (Inception modules)
+# BLOCK - 3 (Inception modules)
 x = inception_module(x, filters_1x1=64, filters_3x3_pre=96, filters_3x3=128, filters_5x5_pre=16,
                      filters_5x5=32, filters_maxpool=32, name='inception_3a')
 x = inception_module(x, filters_1x1=128, filters_3x3_pre=128, filters_3x3=192, filters_5x5_pre=32,
@@ -102,7 +101,7 @@ x = inception_module(x, filters_1x1=128, filters_3x3_pre=128, filters_3x3=192, f
 x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3')(x)
 
 
-# B L O C K - 4
+# BLOCK - 4
 x = inception_module(x, filters_1x1=192, filters_3x3_pre=96, filters_3x3=208, filters_5x5_pre=16,
                      filters_5x5=48, filters_maxpool=64, name='inception_4a')
 
@@ -134,7 +133,7 @@ x = inception_module(x, filters_1x1=256, filters_3x3_pre=160, filters_3x3=320, f
 x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_4_3x3')(x)
 
 
-# B L O C K - 5
+# BLOCK - 5
 x = inception_module(x, filters_1x1=256, filters_3x3_pre=160, filters_3x3=320, filters_5x5_pre=32,
                      filters_5x5=128, filters_maxpool=128, name='inception_5a')
 x = inception_module(x, filters_1x1=384, filters_3x3_pre=192, filters_3x3=384, filters_5x5_pre=48,
@@ -146,13 +145,12 @@ x = Dense(n_categories, activation='softmax', name='output')(x)
 model =  tf.keras.Model(input_layer, [x, x1, x2], name='inception_v1')
 
 
-
-# READING CHECKPOINTS
-
+# OPENING CHECKPOINTS FOLDER
 #Include the epoch in the file name (uses `str.format`)
 checkpoint_path = "train_cp/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
+# SAVING CHECKPOINTS 
 epotch_size = 2561
 frequency = 5
 
@@ -170,20 +168,17 @@ model.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'cat
               loss_weights=[1, 0.3, 0.3], optimizer=sgd, metrics=['accuracy'])
 
 
-
-latest = tf.train.latest_checkpoint(checkpoint_dir)
-
-
 while True:
     name = input('Do you want to use a CHECKPOINT  or start from SCRATCH?\n')
     if name.lower() == "checkpoint":
+        latest = tf.train.latest_checkpoint(checkpoint_dir)
         model.load_weights(latest)
         break
     elif name.lower() == "scratch":
         break
     else:
         pass
-    
+
 history = model.fit(train_generator, epochs=100, validation_data = val_generator, callbacks=[cp_callback], verbose = 2)
 
 # summarize history for loss
